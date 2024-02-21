6b9aa7dbd29f7ec569e0c0e3bc26503470282e9f
Cat-T0m <153538022+Cat-T0m@users.noreply.github.com>
2024-02-21 02:01:59 +0200

    Add files via upload
    The archive skin_disease_50 contains the notebook file and the data organized in the correct folder structure to run the analysis on the data set containing 50 images per class, with a resolution of 128 by 128 pixels. The archive skin_disease_100 contains the relevant files for the dataset containing 100 images per class (with 2 exceptions). You can also find the notebook files separately, as well as some small python scripts for splitting the data into training and test (same as validation in this case) folders, for the VGG and Inception algorithms.

---
+++ split_dermatitis.py
@@ -0,0 +1,51 @@
+import os, shutil, random
+
+# preparing the folder structure
+
+split_percentage = 90
+
+images_path = 'data2/Dermatitis/'
+    
+    
+training_images_path = 'skind_training2/Dermatitis/'
+validation_images_path = 'skind_validation2/Dermatitis/'
+    
+os.mkdir(training_images_path)
+os.mkdir(validation_images_path)
+
+files = []
+
+
+
+for r, d, f in os.walk(images_path):
+    for file in f:
+        strip=file[0:len(file)]
+        files.append(strip)
+
+random.shuffle(files)
+
+size = len(files)                   
+
+split = int(split_percentage * size / 100)
+
+print(files[split])
+print("copying training data")
+for i in range(split):
+    image_file = files[i] 
+    print(images_path)
+    print(image_file)
+    src_image = images_path + image_file
+    shutil.copy(src_image, training_images_path) 
+                         
+    
+print("copying validation data")
+for i in range(split, size):
+    f = files[i]
+                         
+    image_file = f
+    src_image = images_path + image_file
+    shutil.copy(src_image, validation_images_path) 
+                         
+    
+    
+print("finished")
